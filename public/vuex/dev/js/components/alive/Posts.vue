e91e2645eb08d377dcba6797523165cc06fe6f2f
zhuziliu <zhuziliu@b5m.com>
2016-07-29 07:57:17 +0800

    add

---
+++ public/vuex/dev/js/components/alive/Posts.vue
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+	export default {
+		activate: function(done){
+			console.log(222222222);
+			done();
+		}
+	}
+</script>
+
+<template>
+	<h1>Posts</h1>
+</template>
